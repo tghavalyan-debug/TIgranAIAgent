bd6b6a7cceb17237cff45f3ca8ef887c5b321546
Tigran <tigran.ghavalyan@softacademy.am>
2026-06-05 20:32:31 +0400

    lol

--- Loader.py
+++ Loader.py
@@ -1,5 +1,17 @@
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain_chroma import Chroma
+
 import os
+from dotenv import load_dotenv
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+
 
 def load_text_file(file_path: str, encoding: str = "utf-8"):
     """
@@ -13,7 +25,12 @@ def load_text_file(file_path: str, encoding: str = "utf-8"):
         list: List of Document objects containing text and metadata.
     """
     try:
-        loader = TextLoader(file_path, encoding=encoding)
+        loader = DirectoryLoader(
+    "data/",
+    glob="*.txt",
+    loader_cls=TextLoader,
+    loader_kwargs={"encoding": "utf-8"}
+)
         documents = loader.load()
         return documents
     except FileNotFoundError:
@@ -23,13 +40,39 @@ def load_text_file(file_path: str, encoding: str = "utf-8"):
     except Exception as e:
         print(f"Unexpected error: {e}")
 
+
+def build_store(documents):
+
+    splitter = CharacterTextSplitter(chunk_size=200, separator=".")
+
+    chunks = splitter.split_documents(documents)
+
+    load_dotenv()
+
+    for chunk in chunks:
+        print(len(chunk.page_content))
+
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+    model="text-embedding-004")
+
+    db = Chroma.from_documents(
+      documents=chunks,
+      embedding=embeddings,
+      persist_directory="data/chroma"
+)
+
+
+
 if __name__ == "__main__":
-    # Example usage
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Joins it cleanly to 'data/privatedata.txt'
     file_path = os.path.join(script_dir, "data", "privatedata.txt")
+    print(file_path)
+
     docs = load_text_file(file_path)
+
 
     if docs:
         print(f"Loaded {len(docs)} document(s).")
@@ -37,8 +80,5 @@ if __name__ == "__main__":
         print(docs[0].page_content[:200])  # Show first 200 characters
         print("Metadata:", docs[0].metadata)
 
-#from langchain_community.document_loaders import DirectoryLoader
+    build_store(docs)
 
-#loader = DirectoryLoader("data/", glob="*.txt", loader_cls=TextLoader)
-#docs = loader.load()
-#print(f"Loaded {len(docs)} text files.")
